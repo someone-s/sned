1507bafb5b9e65c288907660430f43a16d8c8957
someone-s <workemaileden@gmail.com>
2025-08-26 10:14:17 +0100

    fix milepost

--- Assets/Scripts/Milepost.cs
+++ Assets/Scripts/Milepost.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class Milepost : MonoBehaviour
 {
+    private readonly WaitForSeconds waitForSeconds = new(3f);
+
     public IEnumerator OnTriggerEnter2D(Collider2D collider)
     {
         if (enabled == false) yield break;
-        enabled = false;
 
-        yield return new WaitForSeconds(3f);
+        yield return waitForSeconds;
 
         if (!collider.IsTouching(GetComponent<Collider2D>())) yield break;
+
+        enabled = false;
 
         ProgressControl.Instance.SetProgress(transform.position.y);
         GetComponent<Collider2D>().enabled = false;
